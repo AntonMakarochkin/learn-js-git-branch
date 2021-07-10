1b640d4b3ceca4d1b7ef0c744e68ecf0feab5988
UTonyFast <anton29mak@mail.ru>
2021-07-10 17:58:18 +0300

    function

--- main.js
+++ main.js
@@ -1,5 +1,33 @@
-let browser = 'Chrome'
 
-if (browser == 'Edge') alert( "You've got the Edge!" ); 
-else if (browser == 'Chrome' || browser == 'Firefox' || browser == "Safari" || browser == "Opera")  alert( "Okay we support these browsers too" ); 
-else  alert( 'We hope that this page looks ok!' );
+function checkAge(age) {
+  return (age > 18) ? true : confirm('Родители разрешили?')
+}
+
+function min(a, b) {
+  if ( a < b) {
+    return a
+  } else {
+    return b
+  }
+} 
+
+
+function max(c, d) {
+  return c < d ? d : c;
+}
+
+function pow(x, n) {
+  let result = x
+  for(i=1; i<n; i++) {
+    result *=x
+  }
+  return result
+}
+
+let x = prompt("x?", '');
+let n = prompt("n?", '');
+if (n < 1) {
+  alert(`Степень ${n} не поддерживается, используйте натуральное число`);
+} else {
+  alert( pow(x, n) );
+}
