9f57e90506b82bfaf6a70eeebc155ff1e922bf68
UTonyFast <anton29mak@mail.ru>
2021-07-10 15:05:58 +0300

    first

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
-let browser = 'Chrome'
+/* let browser = 'Chrome'
 
 if (browser == 'Edge') alert( "You've got the Edge!" ); 
 else if (browser == 'Chrome' || browser == 'Firefox' || browser == "Safari" || browser == "Opera")  alert( "Okay we support these browsers too" ); 
-else  alert( 'We hope that this page looks ok!' );
+else  alert( 'We hope that this page looks ok!' ); */
+
+
+
+
+/* const number = +prompt('Введите число между 0 и 3', "")
+
+switch(number) {
+  case 0 :
+    alert('number 0')
+  break
+
+  case 1 :
+    alert('number 1')
+  break
+
+  case 3 : 
+  case 2 :
+    alert('number 3 or 2')
+  break
+} */
