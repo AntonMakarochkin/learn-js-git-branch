f15096e11b6d1619ba6878ca80f2d6b66fdb8d6f
UTonyFast <anton29mak@mail.ru>
2021-07-13 18:00:48 +0300

    type-of-Array

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+/* 
 function checkAge(age) {
   return (age > 18) ? true : confirm('Родители разрешили?')
 }
@@ -50,4 +50,110 @@ let calculator = {
 calculator.read()
 
 console.log(calculator.sum())
-console.log(calculator.mul())
+console.log(calculator.mul()) */
+
+/* function Calculator() {
+  this.read = function() {
+    this.firstNumber = +prompt('first', '')
+    this.secondNumber = +prompt('second', '')
+  }
+  this.sum = function() {
+    return this.firstNumber + this.secondNumber
+  }
+   this.multiple = function() {
+     return this.firstNumber * this.secondNumber
+   }
+
+  
+}
+
+let calculator = new Calculator()
+
+calculator.read() */
+
+/* 
+function Accumulator(staringValue) {
+  this.value = staringValue
+
+  this.read = function() {
+    this.value += +prompt('first', '')
+  }
+}
+
+let accum = new Accumulator(2)
+
+accum.read()
+accum.read()
+
+console.log(accum.value)
+
+ */
+
+
+
+/* let target = 'xxx'
+let targetV = 'viagra'
+function checkSpam(str) {
+  let checkStr = str.toLowerCase()
+  pos = 0
+  return checkStr.includes(target) || checkStr.includes(targetV)
+}
+
+console.log(checkSpam('asdasd vIagra asdasd')) */
+
+
+
+/* function truncate(str, maxlength) {
+  let strLenght = str.length 
+ if (strLenght > maxlength) return str.slice(0, maxlength - 1) + '...' 
+ str
+}
+
+
+console.log(truncate('lova bumbastic imato lova lova bumbastic', 10)) */
+
+
+/* function extractCurrencyValue(str) {
+ return +str.slice(1)
+ }
+ console.log(extractCurrencyValue('#100'))
+
+ */
+
+
+
+
+
+/* function sumInput(){
+   let arr = []
+
+   while(true) {
+     let number = prompt('n', '')
+     if (number === '' || number === null || !isFinite(number)) break
+     arr.push(+number)
+   }
+
+   let sum = 0
+   for (let key of arr) {
+     sum += key
+   }
+   return sum
+  }
+alert( sumInput() );   */
+
+
+
+/* function getMaxSubSum(arr) {
+  maxSum = 0
+  currentNumber = 0
+
+  for (let key of arr) {
+    currentNumber += key
+    if (currentNumber < 0) currentNumber = 0
+    maxSum = Math.max(currentNumber, maxSum)
+  }
+  return maxSum
+
+
+}
+console.log( getMaxSubSum([-1, 2, 3, -9, 2, 4]) ); // 6 */
